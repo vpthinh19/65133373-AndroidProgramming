41de7da96804e3396d6d39fc90f0b47e2e9e6bfc
vpthinh19 <viphucthinh03@gmail.com>
2026-04-05 11:28:33 +0700

    update dynamicfragment

---
+++ DynamicFragment/app/src/main/java/com/vpt/dynamicfragment/MainActivity.java
@@ -0,0 +1,30 @@
+package com.vpt.dynamicfragment;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction()
+                    .add(R.id.contentContainer, new ContentFragment())
+                    .add(R.id.footerContainer, new FooterFragment())
+                    .commit();
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
