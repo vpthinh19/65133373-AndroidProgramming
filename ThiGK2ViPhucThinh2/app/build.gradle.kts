67640cb682d7233189f5702681c3a35312153f6e
vpthinh19 <viphucthinh03@gmail.com>
2026-04-20 08:24:37 +0700

    update lan 1

--- ThiGK2ViPhucThinh2/app/build.gradle.kts
+++ ThiGK2ViPhucThinh2/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.thigk2.viphucthinh"
-        minSdk = 35
+        minSdk = 29
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -40,7 +40,6 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
